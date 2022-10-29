693e00732b54ef009d82f544eb53ded6e53ac716
Matthew D Webb <matt.d.webb@icloud.com>
2022-10-29 12:28:00 +0100

    fix: update rating type on mock data (#9)

--- prisma/mocks.ts
+++ prisma/mocks.ts
@@ -1,4 +1,4 @@
-import { Federation } from "@prisma/client";
+import { Federation, GameType } from "@prisma/client";
 /**
  * Mock data of the 2022-23 Wharfedale Junior League
  * Example: https://ecflms.org.uk/lms/node/64377/tables
@@ -145,94 +145,112 @@ export const players = [
 export const ratings = [
   // Mountain Lions
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1154,
     playerId: 1
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 906,
     playerId: 2
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 792,
     playerId: 3
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 507,
     playerId: 4
   },
   // Night Hawks
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 728,
     playerId: 5
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 749,
     playerId: 6
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 702,
     playerId: 7
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 397,
     playerId: 8
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 228,
     playerId: 9
   },
   // Leopards
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1527,
     playerId: 10
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1306,
     playerId: 11
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1178,
     playerId: 12
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1223,
     playerId: 13
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 742,
     playerId: 14
   },
   // Orangutans
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1188,
     playerId: 15
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 1088,
     playerId: 16
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 669,
     playerId: 17
   },
   {
+    type: GameType.rapidplay,
     federation: Federation.ECF,
     rating: 369,
     playerId: 18
